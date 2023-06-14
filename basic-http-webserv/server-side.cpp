aaad2d351efff59cbf518778c873dfd14a071e78
Rahhal Rizqy <rarahhal@e1r4p3.1337.ma>
2023-06-14 18:32:22 +0100

    simple networking without multiplexing

--- basic-http-webserv/server-side.cpp
+++ basic-http-webserv/server-side.cpp
@@ -6,25 +6,19 @@
 /*   By: rarahhal <rarahhal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 17:33:12 by rarahhal          #+#    #+#             */
-/*   Updated: 2023/06/06 17:48:16 by rarahhal         ###   ########.fr       */
+/*   Updated: 2023/06/14 18:23:45 by rarahhal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+// #include "switch-data.hpp"
 #include <iostream>
 #include <string>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
-// read
-#include <sys/types.h>
-#include <sys/uio.h>
 #include <unistd.h>
+#include <fstream>
 
-// open
-#include <fcntl.h>
-
-#define PORT 8080
+#define PORT 4875
 
 int main(void)
 {
@@ -42,6 +36,11 @@ int main(void)
     if (server_fd < 0)
         std::cout << "ERROR in creation socket\n";
 	
+	int reuse = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+    	std::cout << "ERROR setting socket options\n";
+	}
+
 	if (bind(server_fd, (struct sockaddr *)&adderss, sizeof(adderss)) < 0)
 		std::cout << "ERROR in binding socket\n";
 	
@@ -50,18 +49,27 @@ int main(void)
 
 	long valread;
 
-	int file = open("index.html", O_RDONLY, 666);
-	if (file < 0)
-		std::cout << "Error in opning file\n";
-	char buffer[1563];
-	int size =  read(file, buffer, 1563);
-	if (size < 0)
-		std::cout << "Error in raeding file\n";
-	close(file);
-	std::string headers = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 1563\n\n";
-	std::string response = headers + buffer;
-	// hello.append(buffer);
-	
+	std::ifstream file("videoplayback.mp4", std::ifstream::binary);
+
+	if (!file.is_open())
+	{
+		std::cout << "Error in opening file\n";
+		std::exit(-404);
+	}
+	file.seekg(0, std::ios::end);
+    int videoLength = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+	char uffer[videoLength/2];
+	file.read(uffer, videoLength/2);
+	char uffer2[videoLength/2];
+	file.read(uffer2, videoLength/2);
+
+	file.close();
+	char headers[66] = "HTTP/1.1 200 OK\nContent-Type: video/mp4\nContent-Length: ";
+	std::string header = headers + std::to_string(videoLength) + "\n\n";
+	std::string response = header + uffer + uffer2;
+
 	while (1)
 	{
 		std::cout << "\n++++++ Waiting for new connection +++++++\n\n";
@@ -72,8 +80,14 @@ int main(void)
 		}
 		char buffer[30000] = {0};
 		valread = read(new_socket, buffer, 30000);
+		
 		std::cout << buffer << std::endl;
-		write(new_socket, response.c_str(), response.size());
+		
+		send(new_socket, header.c_str(), header.size(), MSG_SEND);
+		send(new_socket, uffer, videoLength/2, MSG_SEND);
+		send(new_socket, uffer2, videoLength/2, MSG_SEND);
+		// write(new_socket, headers.c_str(), response.size());
+		// write(new_socket, uffer, 3465);
 		std::cout << "-------------------response message sent---------------\n";
 		std::cout << response << std::endl;
 		std::cout << "------------------------------------------------------\n";
