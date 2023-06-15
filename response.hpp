e7053db26361601f44ea42dc93bc86b954f62689
Rahhal Rizqy <rarahhal@e1r13p6.1337.ma>
2023-06-15 16:39:53 +0100

    save

--- response.hpp
+++ response.hpp
@@ -6,7 +6,7 @@
 /*   By: rarahhal <rarahhal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 21:47:25 by rarahhal          #+#    #+#             */
-/*   Updated: 2023/06/14 22:26:07 by rarahhal         ###   ########.fr       */
+/*   Updated: 2023/06/15 16:39:39 by rarahhal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,19 @@
 class Response
 {
     private:
-        std::string _version;
+		std::string _version;
         int _status_code;
         std::string _status_message;
-        // std::vector<std::string> _header;
-        std::map<std::string, std::vector<std::string> > _headers;
+        std::map<std::string, std::string> _headers;
 		std::string _body;
+		std::string generateStatusLine();
+		std::string generateHeaders();
     public:
 		Response();
 		void setVersion(std::string const &version);
 		void setStatusMessage(std::string const &status_message);
 		void setStatusCode(int status_code);
 		void setHeader(std::string &name, std::string const &value);
+		std::string generateResponse();
 		~Response();
 };
