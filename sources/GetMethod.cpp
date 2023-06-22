d952e6b9e20a119e4f6945088de9ae8423786b46
Rahhal Rizqy <rarahhal@e1r7p2.1337.ma>
2023-06-23 00:12:35 +0100

    in the true way in cha2 lah

--- sources/GetMethod.cpp
+++ sources/GetMethod.cpp
@@ -6,7 +6,7 @@
 /*   By: rarahhal <rarahhal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 15:24:42 by rarahhal          #+#    #+#             */
-/*   Updated: 2023/06/22 19:03:18 by rarahhal         ###   ########.fr       */
+/*   Updated: 2023/06/22 22:51:57 by rarahhal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,11 @@
     
 // }
 
-// std::string Response::CheckWhichRequestMethod(Server server, Request request) {
-//     if (request.getMethod() == "GET")
-//         Response::GetMethod(server, request);
-//     // else if (request.getMethod() == "POST")
-//     //     Response::PostMethod();
-//     // else if (request.getMethod() == "DELETE")
-//     //     Response::DeleteMethod();
-//     else
-//         std::cout << "IF SHOWING THIS LINE IT IS A PROBLEME BEFORE WORKING ON THE REQUEST METHOD !!!!!!!!!\n";
-// }
-
 void Response::GetMethod(Server server, Request request) {
     // (void)server;
     // (void)request;
 
     // !!!!!!!!!!!!!! STOP HERE but path need more and more handling !!!!!!!!!!!!!!!
+    _requestedSource = GetRequestedSource(server._locations[Response::_matchedLocationPosition], request.getUri());
     std::cout << "THE EXISTING PATH: " << GetRequestedSource(server._locations[Response::_matchedLocationPosition], request.getUri()) << std::endl;
 }
